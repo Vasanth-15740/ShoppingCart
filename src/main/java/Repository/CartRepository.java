4f90ffe215455366060d01fbc72b55299f1ef1a1
vasanth <vasanth15740@gmail.com>
2025-05-29 12:22:44 +0530

    modified

--- src/main/java/Repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import Model.Cart;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.Optional;
-public interface CartRepository extends MongoRepository<Cart, String> {
-    Optional<Cart> findByUserId(String userId);
-}
